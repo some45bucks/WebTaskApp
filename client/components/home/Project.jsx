7264a5a0ee6f0dfe9342c695d377a1eab92b13e9
Kailey <llamabyter@gmail.com>
2022-02-17 14:26:11 -0700

    more CSS stuff

--- client/components/home/Project.jsx
+++ client/components/home/Project.jsx
@@ -2,62 +2,59 @@ import { useContext, useEffect, useState } from 'react';
 import { ApiContext } from '../../utils/api_context';
 import { Button } from '../common/button';
 
-export const Project = ({project,addUser}) => {
-    const api = useContext(ApiContext);
-    const [email, setEmail] = useState('');
-    const [lead, setlead] = useState({firstName: "Loading..."});
-    const [users, setUsers] = useState([]);
+export const Project = ({ project, addUser }) => {
+  const api = useContext(ApiContext);
+  const [email, setEmail] = useState('');
+  const [lead, setlead] = useState({ firstName: 'Loading...' });
+  const [users, setUsers] = useState([]);
 
-    useEffect(async () => {
-        update()
-      
-    }, []);
+  useEffect(async () => {
+    update();
+  }, []);
 
-    const update = async ()=> 
-    {
-      if(project)
-        {
-          const {lead} = await api.get(`/projects/${project.id}/lead`);
+  const update = async () => {
+    if (project) {
+      const { lead } = await api.get(`/projects/${project.id}/lead`);
 
-          if(lead)
-          {
-            setlead(lead);
-          }
-          
+      if (lead) {
+        setlead(lead);
+      }
 
-          const {users} = await api.get(`/projects/${project.id}/default`)
-          setUsers(users);
-        }
+      const { users } = await api.get(`/projects/${project.id}/default`);
+      setUsers(users);
     }
+  };
 
-    return (
-      <div className="border-2 rounded p-4">
-        <div>Project name: {project.name}</div>
-        <div>Project id: {project.id}</div>
-        <div>Lead: {lead.firstName}</div>
-        
-        <div>
+  return (
+    <div className="flex-1 border-2 rounded p-2 h-max bg-blue-500">
+      <div>Project name: {project.name}</div>
+      <div>Project id: {project.id}</div>
+      <div>Lead: {lead.firstName}</div>
+
+      <div>
         Other Users:
         {users.map((user) => {
-          
-          if(user.id !== lead.id)
-          {
-            return (
-              <div key={user.id}> 
-                {user.firstName}
-              </div>)
+          if (user.id !== lead.id) {
+            return <div key={user.id}>{user.firstName}</div>;
           }
-          
         })}
-        </div>
-        <div>
+      </div>
+      <div>
           {/* This is where the task button is */}
-          <Button onClick={() => console.log(project.id)}>Add Task</Button>
+          <div> <Button onClick={() => console.log(project.id)}>Add Task</Button> </div>
 
-          <label htmlFor="emailEnter" >User Email:</label>
-          <input className="border-2" id="emailEnter" value={email} onChange={(e)=> setEmail(e.target.value)} type="text"/>
-          <Button onClick={() => addUser(project.id,email,update)}>Add User</Button>
+          <div> <Button onClick={() => addUser(project.id, email, update)}>Add User</Button> </div>
+          <div>
+        <label htmlFor="emailEnter">User Email:</label>
+        <input
+          className="border-2"
+          id="emailEnter"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+        />
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
--- client/components/home/_home.jsx
+++ client/components/home/_home.jsx
@@ -12,41 +12,52 @@ export const Home = () => {
   const api = useContext(ApiContext);
 
   const [projects, setProjects] = useState([]);
-  
 
   useEffect(async () => {
     resetProjects();
   }, []);
 
-  const resetProjects = async ()=>{
+  const resetProjects = async () => {
     const { projects } = await api.get('/projects');
     setProjects(projects);
-  }
+  };
 
-  const createProject = async ()=>{
+  const createProject = async () => {
     const { project } = await api.post('/projects');
     setProjects([...projects, project]);
-  }
+  };
 
-  const addUser = async (id,email,update)=>{
+  const addUser = async (id, email, update) => {
     const emailBod = {
-      email: email
+      email: email,
     };
-    await api.post(`/projects/${id}`,emailBod)
+    await api.post(`/projects/${id}`, emailBod);
     update();
-  }
+  };
 
-  
-  
   return (
-    <div className="p-4">
-      <Button onClick={()=>{createProject()}}>Create New Project</Button>
-        {projects.map((project) => {
-          return (
-          <div key={project.id}> 
-          <Project project={project} addUser={addUser}/>
-          </div>
-        )})}
+    <div className="bg-blue-200">
+      <div className="bg-blue-900/90 mr-1">
+        <Button
+          onClick={() => {
+            createProject();
+          }}
+        >
+          Create New Project
+        </Button>{' '}
+      </div>
+      <div className="flex flex-row h-full">
+        <div className="bg-blue-900/90 flex-none w-1/6 mr-1 flex-shrink-0"> side bar </div>
+        <div className="flex flex-wrap rounded m-1 max-h-screen overflow-y-auto">
+          {projects.map((project) => {
+            return (
+              <div key={project.id}>
+                <Project project={project} addUser={addUser} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
