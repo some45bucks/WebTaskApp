a2d58e896ad8aa6816b0389921e3bb52aca9027d
Brennan Edgley <bredgley@gmail.com>
2022-02-20 16:04:10 -0700

    Changes to format tasks

--- client/components/home/Task.jsx
+++ client/components/home/Task.jsx
@@ -1,8 +1,9 @@
 import { Button } from '../common/button';
+import { Task } from '../common/task';
 export const Task = ({task,completeTask,addUser}) => {
 
     return(
-        <div className="flex-1 border-2 rounded p-2 m-2 bg-blue-500">
+        <div className={`flex-1 text-left border-2 rounded p-2 m-2 bg-${(task.status) ? 'green':'red'}-500`}>
             <div>{task.title}</div>
             <div>{task.timeEst}</div>
             <div>{task.description}</div>
