cd1b54fe215bcec84d805030e24dcfa3529983c4
Brennan Edgley <bredgley@gmail.com>
2022-02-20 14:56:31 -0700

    Added a way to give a project a name and tasks their necessary info.

--- client/components/home/Project.jsx
+++ client/components/home/Project.jsx
@@ -6,17 +6,11 @@ import { home } from './_home';
 
 
 export const Project = ({ project, myOnClick, isSelected}) => {
-  const api = useContext(ApiContext);
-  const [projectName, setProjectName] = useState(project.name)  
+  const api = useContext(ApiContext); 
 
   return (
     <div className={`flex-1 border-2 rounded p-2 m-2 bg-${(isSelected) ? 'red':'blue'}-500`} onClick={()=> myOnClick(project)}>
-      <Button onClick={() => home.addProjectName(project.id, projectName)}>Change Name</Button>
-      <label>Project Name:
-        <input
-          defaultValue={projectName}
-          onChangeText={(e) => setProjectName(e.target.value)}
-          type="text"/>
+      <label>Project Name: {project.name}
       <div>Project id: {project.id}</div>
       </label>
     </div>
--- client/components/home/Task.jsx
+++ client/components/home/Task.jsx
@@ -3,42 +3,11 @@ export const Task = ({task,completeTask,addUser}) => {
 
     return(
         <div className="flex-1 border-2 rounded p-2 m-2 bg-blue-500">
-            <div><input
-                className="border-2"
-                id="idEnter"
-                defaultValue={'task number: ' + task.id}
-                // value = {id}
-                // onChange={(e) => setId(e.target.value)} 
-                type="text"/>
-            <input
-                className="border-2"
-                id="titleEnter"
-                defaultValue={task.title}
-                // value = {title}
-                // onChange={(e) => setTitle(e.target.value)} 
-                type="text"/> <Button onClick={()=>addTask(id, )}>Update Task</Button> </div>
-            <input
-                className="border-2"
-                id="descriptionEnter"
-                defaultValue={'description'}
-                // value = {description}
-                // onChange={(e) => setDescription(e.target.value)} 
-                type="text"/>
-            <input
-                className="border-2"
-                id="timeEstEnter"
-                defaultValue={'time estimate: ' + task.timeEst}
-                // value = {timeEst}
-                // onChange={(e) => setTimeEst(e.target.value)} 
-                type="text"/>
+            <div>{task.title}</div>
+            <div>{task.timeEst}</div>
             <div>{task.description}</div>
-            <div>
-            <input
-                className="border-2"
-                defaultValue={''}
-                
-                type="text"/>
-            <Button onClick={()=>addUser(task)}>Add User</Button></div>
+            <Button onClick={()=>addUser(task)}>Add User</Button> 
+            <Button onClick={()=>completeTask(task)}>Complete Task</Button>
         </div>
     )
 }
