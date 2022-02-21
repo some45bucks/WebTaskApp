127da2c67b82fea29a79f83d94801b6ee9c10a08
Kailey <llamabyter@gmail.com>
2022-02-20 17:01:15 -0700

    got changing task finished

--- client/components/home/Task.jsx
+++ client/components/home/Task.jsx
@@ -2,12 +2,13 @@ import { Button } from '../common/button';
 export const Task = ({task,completeTask,addUser}) => {
 
     return(
-        <div className={`flex-1 text-left border-2 rounded p-2 m-2 bg-${(task.status) ? 'green':'red'}-500`}>
+        <div className={`flex-1 text-left border-2 rounded p-2 m-2 `}>
             <div> Task ID: {task.id}</div>
             <div> Task Title: {task.title}</div>
             <div> Description: {task.description}</div>
             <div> Time Estimate: {task.timeEst}</div>
-            <div> Task Status: {task.status} </div>
+            <div> Task Status:
+            {(task.status) ? <div className="bg-green-500 w-1/6"> Complete! </div> : <div className='bg-red-500  w-1/4'> Not Complete! </div>} </div>
         
             <Button onClick={()=>addUser(task)}>Add User</Button> 
             <Button onClick={()=>completeTask(task)}>Complete Task</Button>
