62cd65a2fc95a087afde7fafa9a00a67d4f9f8c7
Kailey <llamabyter@gmail.com>
2022-02-21 12:07:13 -0700

    add ID to user card

--- client/components/home/User.jsx
+++ client/components/home/User.jsx
@@ -6,6 +6,7 @@ export const User = ({user,lead,setFocus,isSelected})=>{
     const api = useContext(ApiContext);
     return (
         <div className={`flex-1 border-2 rounded p-2 m-2 bg-${(isSelected) ? 'blue-200':'blue-500'}`} onClick={()=>setFocus(user)}>
+            <div> ID: {user.id} </div>
            {(user.id == lead.id) ? 'Lead': 'User'} {user.firstName}
         </div>
     )
